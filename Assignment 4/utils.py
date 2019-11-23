ccf55d62067ae3209b9a7dd88e5858985c222fd8
Jingbo Liu <JingboLiu@JingboLiudeMacBook-Pro.local>
2019-11-23 16:49:38 -0500

    assignment 4 finished

--- Assignment 4/utils.py
+++ Assignment 4/utils.py
@@ -29,7 +29,10 @@ def pad_sents(sents, pad_token):
     sents_padded = []
 
     ### YOUR CODE HERE (~6 Lines)
-
+    max_len = max(len(sent) for sent in sents)
+    for sent in sents:
+        sent_len = len(sent)
+        sents_padded.append(sent + (max_len-sent_len)*[pad_token])
 
     ### END YOUR CODE
 
